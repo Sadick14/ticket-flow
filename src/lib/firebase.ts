2c387893e039736286ff14e40562a5259202789f
Issaka Saddick <issakasaddick14@gmail.com>
2025-07-09 23:09:19 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCioz6RGKlmYc3aIYpEWTCTYJFwtf0kDaA",
+  authDomain: "ticketflow-5bf3g.firebaseapp.com",
+  projectId: "ticketflow-5bf3g",
+  storageBucket: "ticketflow-5bf3g.appspot.com",
+  messagingSenderId: "618525371848",
+  appId: "1:618525371848:web:ea66cbc638516eb579a4fd"
 };
 
 // Initialize Firebase
